e2b6c92246e77ec7b80840822f0f79c6f6da6f58
Nicolae N <35431660+niklaus17@users.noreply.github.com>
2026-09-15 16:42:12 +0300

    Update Google Apps Script endpoint

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CAMPAIGNSHEET_CONFIG = {
   dataSource: "auto",
-  googleApiUrl: "https://script.google.com/macros/s/AKfycbzgHnAFW2bPErREkEGFctP1YrgNiftix-NKY_XcXCnl3gPikNl6kVKXB4vjRWikyItzlA/exec",
+  googleApiUrl: "https://script.google.com/macros/s/AKfycbxHMmGWw1VyuF5KMetiQzW5Ch_Gr4azBYR1vsGjtr4qhEVsFsjr14V5WwQQskAyVKjq4g/exec",
   googleApiToken: "",
   defaultCurrency: "USD",
   allTimeFrom: "2026-01-01",
